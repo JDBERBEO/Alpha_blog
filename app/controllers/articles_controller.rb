7b03aa10ff00023bbb72b8afbb177b6fdf7f6aa5
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-10-04 09:08:07 -0500

    feat: POST new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # render plain: params[:article]
+    # whitelisting?? class 90
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+    #redirect_to article_path(@article)
+    flash[:notice] = "Article was created successfully!"
+    redirect_to @article
+    else 
+      render 'new'
+    end
+  end
+
 end
