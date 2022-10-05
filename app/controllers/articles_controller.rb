0f83a97947db6ae9d28b323fcac61a9993415d12
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-10-05 09:28:25 -0500

    feat: creates update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # render plain: params[:article]
     # whitelisting?? class 90
@@ -24,4 +28,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] ="Article was updated succesfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
