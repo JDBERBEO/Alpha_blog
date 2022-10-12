6553aad36a07dfe8fa52569336b84bfc05706e69
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-10-12 09:03:48 -0500

    feat: refactor new and edit template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
   def show 
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,13 +13,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
     # render plain: params[:article]
     # whitelisting?? class 90
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
     #redirect_to article_path(@article)
     flash[:notice] = "Article was created successfully!"
@@ -29,8 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] ="Article was updated succesfully"
       redirect_to @article
     else
@@ -39,8 +38,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
